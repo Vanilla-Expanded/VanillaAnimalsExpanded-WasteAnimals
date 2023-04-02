1fdb4fda821293504d99b2135bcfbeea086ef9f7
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-04-02 17:01:47 +0200

    v0.1

---
+++ 1.4/Source/VanillaAnimalsExpandedWaste/VanillaAnimalsExpandedWaste/Damages/DamageWorker_RipPart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanillaAnimalsExpandedWaste.Damages
+{
+    internal class DamageWorker_RipPart
+    {
+    }
+}
