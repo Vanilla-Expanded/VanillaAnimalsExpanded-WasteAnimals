15e08d9f7a43131cb0076e86b32323be9faa7c3d
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-04-09 16:41:50 +0200

    Fixes

---
+++ 1.4/Source/VanillaAnimalsExpandedWaste/VanillaAnimalsExpandedWaste/Damages/DamageWorker_Carcinoma.cs
@@ -0,0 +1,39 @@
+﻿using RimWorld;
+using System;
+using Verse;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace VanillaAnimalsExpandedWaste
+{
+    public class DamageWorker_Carcinoma : DamageWorker_AddInjury
+    {
+        public override DamageWorker.DamageResult Apply(DamageInfo dinfo, Thing victim)
+        {
+
+            Pawn pawn = victim as Pawn;
+
+            if (pawn != null)
+            {
+
+                BodyPartRecord bodyPartRecord = ReflectionCache.GetExactPartFromDamageInfo(this, dinfo, pawn);
+
+                if (bodyPartRecord != null)
+                {
+
+                    pawn.health.AddHediff(HediffDefOf.Carcinoma, bodyPartRecord);
+                }
+            }
+
+
+            DamageWorker.DamageResult damageResult = base.Apply(dinfo, victim);
+
+
+            return damageResult;
+        }
+
+
+    }
+}
+
