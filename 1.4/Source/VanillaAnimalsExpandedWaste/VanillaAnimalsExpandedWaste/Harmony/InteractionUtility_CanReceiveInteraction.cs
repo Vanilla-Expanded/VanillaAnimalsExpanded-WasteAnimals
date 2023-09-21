2cdfdb96b41c09395c8bb8d442a419316ce105b8
SargBjornson <sarg.auxiliar@gmail.com>
2023-09-21 08:27:33 +0200

    v1.03

--- 1.4/Source/VanillaAnimalsExpandedWaste/VanillaAnimalsExpandedWaste/Harmony/InteractionUtility_CanReceiveInteraction.cs
+++ 1.4/Source/VanillaAnimalsExpandedWaste/VanillaAnimalsExpandedWaste/Harmony/InteractionUtility_CanReceiveInteraction.cs
@@ -22,29 +22,39 @@ namespace VanillaAnimalsExpandedWaste
         {
             if (initiator.health?.hediffSet?.HasHediff(InternalDefOf.VAEWaste_Toxflu)==true && recipient.health?.hediffSet?.HasHediff(InternalDefOf.VAEWaste_Toxflu)!=true)
             {
-                float chance = 0.1f;
-                if (initiator.health?.hediffSet?.GetFirstHediffOfDef(InternalDefOf.VAEWaste_Toxflu).Severity < 0.66)
-                {
-                    chance = 0.5f;
-                }
                 System.Random random = new System.Random();
-                double randomChance = random.NextDouble();
-                if (randomChance < chance)
+                double randomChanceToxResistance = random.NextDouble();
+
+                if(randomChanceToxResistance > recipient.GetStatValue(StatDefOf.ToxicResistance))
                 {
-                    recipient.health.AddHediff(InternalDefOf.VAEWaste_Toxflu);
+                    float chance = 0.1f;
+                    if (initiator.health?.hediffSet?.GetFirstHediffOfDef(InternalDefOf.VAEWaste_Toxflu).Severity < 0.66)
+                    {
+                        chance = 0.5f;
+                    }
+                    double randomChance = random.NextDouble();
+                    if (randomChance < chance)
+                    {
+                        recipient.health.AddHediff(InternalDefOf.VAEWaste_Toxflu);
+                    }
                 }
+           
             } else if (initiator.health?.hediffSet?.HasHediff(InternalDefOf.VAEWaste_Toxflu) !=true && recipient.health?.hediffSet?.HasHediff(InternalDefOf.VAEWaste_Toxflu) == true)
             {
-                float chance = 0.1f;
-                if (recipient.health?.hediffSet?.GetFirstHediffOfDef(InternalDefOf.VAEWaste_Toxflu).Severity < 0.66)
-                {
-                    chance = 0.5f;
-                }
                 System.Random random = new System.Random();
-                double randomChance = random.NextDouble();
-                if (randomChance < chance)
+                double randomChanceToxResistance = random.NextDouble();
+                if (randomChanceToxResistance > initiator.GetStatValue(StatDefOf.ToxicResistance))
                 {
-                    initiator.health.AddHediff(InternalDefOf.VAEWaste_Toxflu);
+                    float chance = 0.1f;
+                    if (recipient.health?.hediffSet?.GetFirstHediffOfDef(InternalDefOf.VAEWaste_Toxflu).Severity < 0.66)
+                    {
+                        chance = 0.5f;
+                    }
+                    double randomChance = random.NextDouble();
+                    if (randomChance < chance)
+                    {
+                        initiator.health.AddHediff(InternalDefOf.VAEWaste_Toxflu);
+                    }
                 }
             }
 
