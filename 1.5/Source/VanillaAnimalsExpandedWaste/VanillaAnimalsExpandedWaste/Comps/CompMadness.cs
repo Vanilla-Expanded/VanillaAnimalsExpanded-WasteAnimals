48a1238288d7ae1d38e1715506b31dfca34bc0b3
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-03-20 10:59:50 +0100

    1.5 update

---
+++ 1.5/Source/VanillaAnimalsExpandedWaste/VanillaAnimalsExpandedWaste/Comps/CompMadness.cs
@@ -0,0 +1,130 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using System.Linq;
+using RimWorld;
+using static UnityEngine.Random;
+using Verse.AI;
+
+namespace VanillaAnimalsExpandedWaste
+{
+    public class CompMadness : ThingComp
+    {
+
+        public int tickCounter = 0;
+        public int fixedPeriod = 0;
+        public ToxbearState state = ToxbearState.Normal;
+
+
+        public CompProperties_Madness Props
+        {
+            get
+            {
+                return (CompProperties_Madness)this.props;
+            }
+        }
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+
+            if (fixedPeriod == 0)
+            {
+                fixedPeriod = Props.ticksRange.RandomInRange;
+            }
+
+        }
+
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look<int>(ref this.tickCounter, "tickCounter", 0, false);
+            Scribe_Values.Look<int>(ref this.fixedPeriod, "fixedPeriod", 0, false);
+            Scribe_Values.Look<ToxbearState>(ref this.state, "state", ToxbearState.Normal, false);
+           
+
+
+        }
+
+        public override void CompTick()
+        {
+
+            tickCounter++;
+
+            if (state== ToxbearState.Normal && tickCounter >= fixedPeriod)
+            {
+                Pawn pawn = parent as Pawn;
+                if (pawn != null) {
+                    state = ToxbearState.Berserk;
+                    tickCounter = 0;
+                    pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.Berserk, null, true, false,false, null, false);
+                }
+
+            }
+
+            if (state == ToxbearState.Berserk && tickCounter >= Props.ticksBetweenBerserkAndManhunter)
+            {
+                Pawn pawn = parent as Pawn;
+                if (pawn != null)
+                {
+                    state = ToxbearState.Manhunter;
+                    tickCounter = 0;
+
+                    pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.Manhunter, null, true, false, false,null, false);
+                }
+            }
+
+            if (state == ToxbearState.Manhunter && tickCounter >= Props.ticksAfterManhunter)
+            {
+                Pawn pawn = parent as Pawn;
+                if (pawn != null)
+                {
+                    state = ToxbearState.Terminal;
+                    tickCounter = 0;
+
+                    if (pawn.CurJobDef != InternalDefOf.VAEWaste_AttackSelf)
+                    {
+                        pawn.mindState.mentalStateHandler.CurState.RecoverFromState();
+                        pawn.jobs.EndCurrentJob(JobCondition.Succeeded, false);
+                        Job job = JobMaker.MakeJob(InternalDefOf.VAEWaste_AttackSelf, pawn);
+                        pawn.jobs.StartJob(job);
+                    }
+                }
+
+            }
+
+            if (state == ToxbearState.Terminal && tickCounter >= 500)
+            {
+                Pawn pawn = parent as Pawn;
+                if (pawn != null)
+                {
+                    tickCounter = 0;
+
+                    if (pawn.CurJobDef != InternalDefOf.VAEWaste_AttackSelf && pawn.CurJobDef != JobDefOf.AttackMelee)
+                    {
+                        pawn.jobs.EndCurrentJob(JobCondition.Succeeded, false);
+                        Job job = JobMaker.MakeJob(InternalDefOf.VAEWaste_AttackSelf, pawn);
+                        pawn.jobs.StartJob(job);
+                    }
+                }
+                
+
+            }
+
+
+
+
+
+        }
+
+
+    }
+
+
+
+
+
+}
+
+
---
+++ 1.5/Source/VanillaAnimalsExpandedWaste/VanillaAnimalsExpandedWaste/IngestionOutcomeDoers/IngestionOutcomeDoer_Toxic.cs
@@ -0,0 +1,44 @@
+﻿
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+namespace VanillaAnimalsExpandedWaste
+{
+    public class IngestionOutcomeDoer_Toxic : IngestionOutcomeDoer
+    {
+       
+        public float severity = -1f;
+
+        public ChemicalDef toleranceChemical;
+
+        private bool divideByBodySize;
+
+        public bool multiplyByGeneToleranceFactors;
+
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestAmount)
+        {
+            Hediff hediff = HediffMaker.MakeHediff(HediffDefOf.ToxicBuildup, pawn);
+            float effect = (!(severity > 0f)) ? HediffDefOf.ToxicBuildup.initialSeverity : severity;
+            if (divideByBodySize)
+            {
+                effect /= pawn.BodySize;
+            }
+            effect *= Mathf.Max(1f - pawn.GetStatValue(StatDefOf.ToxicResistance), 0f);
+            hediff.Severity = effect;
+            pawn.health.AddHediff(hediff);
+        }
+
+        public override IEnumerable<StatDrawEntry> SpecialDisplayStats(ThingDef parentDef)
+        {
+            if (parentDef.IsDrug && chance >= 1f)
+            {
+                foreach (StatDrawEntry item in HediffDefOf.ToxicBuildup.SpecialDisplayStats(StatRequest.ForEmpty()))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
